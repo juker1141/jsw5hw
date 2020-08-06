dfddbcd5c4839281fda0d5bca6a7220e1cfee9d6
juker1141 <50107434+juker1141@users.noreply.github.com>
2020-08-07 00:59:52 +0800

    update

--- js/all.js
+++ js/all.js
@@ -1,19 +1,27 @@
+
 new Vue({
   el: '#app',
   data: {
     products: [],
-    tempProduct: {
-      imageUrl: [],
-    },
     api: {
       uuid: '8a8058c0-58d2-485b-b7fc-3c9be181cca7',
       path: 'https://course-ec-api.hexschool.io/api/',
     },
+    shoppingCartList: [],
   },
   methods: {
     getProducts() {
-      console.log(111)
+      const url = `${this.api.path}${this.api.uuid}/ec/products`;
+      axios.get(url)
+        .then(res => {
+          console.log(res);
+          this.products = res.data.data;
+        })
     },
+    addToCart(item) {
+      console.log(item);
+      this.shoppingCartList.push(item);
+    }
   },
   created() {
     this.getProducts()
