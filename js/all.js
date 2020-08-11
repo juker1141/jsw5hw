d61539ea23ffe28ee05d28f798631c235b137bb2
juker1141 <50107434+juker1141@users.noreply.github.com>
2020-08-12 01:50:04 +0800

    update

--- js/all.js
+++ js/all.js
@@ -1,27 +1,69 @@
+Vue.component('loading', VueLoading);
 
 new Vue({
   el: '#app',
   data: {
     products: [],
-    tempProduct: [],
+    tempProduct: {
+      num: ''
+    },
     api: {
       uuid: '8a8058c0-58d2-485b-b7fc-3c9be181cca7',
       path: 'https://course-ec-api.hexschool.io/api/',
     },
+    status: {
+      loadingItem: '',
+    },
     shoppingCartList: [],
+    isLoading: false,
   },
   methods: {
-    getProducts() {
-      const url = `${this.api.path}${this.api.uuid}/ec/products`;
+    getProducts(page = 1) {
+      this.isLoading = true;
+      const url = `${this.api.path}${this.api.uuid}/ec/products?page=${page}`;
       axios.get(url)
         .then(res => {
           console.log(res);
+          this.isLoading = false;
           this.products = res.data.data;
-        })
+        }).catch(error => {
+          this.isLoading = false;
+        });
     },
-    addToCart(item) {
-      console.log(item);
-      this.shoppingCartList.push(item);
+    getProduct(id) {
+      this.status.loadingItem = id;
+      this.isLoading = true;
+      const url = `${this.api.path}${this.api.uuid}/ec/product/${id}`;
+      axios.get(url)
+        .then(res => {
+          this.isLoading = false;
+          console.log(res);
+          this.tempProduct = res.data.data;
+          this.$set(this.tempProduct, 'num', 1);
+          this.status.loadingItem = '';
+          $('#productModal').modal('show');
+        }).catch(error => {
+          this.isLoading = false;
+          this.status.loadingItem = '';
+        });
+    },
+    addToCart(id, quantity = 1) {
+      this.isLoading = true;
+      const url = `${this.api.path}${this.api.uuid}/ec/shopping`;
+      const cart = {
+        product: id,
+        quantity,
+      };
+
+      axios.post(url, cart)
+        .then(res => {
+          this.isLoading = false;
+          console.log(res)
+        }).catch(error => {
+          this.isLoading = false;
+          console.log(error.response)
+        });
+
     }
   },
   created() {
